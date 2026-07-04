604668d76efc09b008fbb637b7f1117bb6489b17
mathanrajk <kmathan301@gmail.com>
2026-07-05 00:21:39 +0530

    first commit

---
+++ src/Pages/AdmiPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdmiPage() {
+  return (
+    <div>AdmiPage</div>
+  )
+}
+
+export default AdmiPage
---
+++ src/Pages/ViewAndUpdate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewAndUpdate() {
+  return (
+    <div>ViewAndUpdate</div>
+  )
+}
+
+export default ViewAndUpdate
